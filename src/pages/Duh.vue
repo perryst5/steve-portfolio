6e1ab36f61bd2900a08f046fd020b4f574d5a352
Steve Perry <perryst5@gmail.com>
2020-05-19 17:03:16 -0400

    init commit

---
+++ src/pages/Duh.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    <audio id="duh">
+      <source src="/duh.mp3">
+    </audio>
+    <button onclick="document.getElementById('duh').play();">Duh</button>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Duh'
+  }
+}
+</script>
+
+<style>
+</style>
